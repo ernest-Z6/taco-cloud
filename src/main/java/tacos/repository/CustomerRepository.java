37cf52ad5fb0567d81eb9db7bd00137c2cfd1b48
ernest_Z6 <zhang.ernest@outlook.com>
2021-12-20 22:46:29 +0800

    Add Repository;

---
+++ src/main/java/tacos/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package tacos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import tacos.entity.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+
+}
