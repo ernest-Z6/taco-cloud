5f815658bcf293f3ca8452846d9720a09030bffa
ernest_Z6 <zhang.ernest@outlook.com>
2021-12-21 10:51:29 +0800

    Add Controller.

--- src/main/java/tacos/controller/CustomerController.java
+++ src/main/java/tacos/controller/CustomerController.java
@@ -1,18 +1,31 @@
 package tacos.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+import tacos.entity.Customer;
 import tacos.response.CustomerResponse;
+import tacos.service.CustomerService;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/customer")
 public class CustomerController {
+	
+	CustomerService customerService;
 
 	@GetMapping("/")
 	public CustomerResponse getCustomer() {
 		return new CustomerResponse("John", "John.Ross@dbschenker.com");
+	}
+	
+	@GetMapping("/all")
+	public List<Customer> getAllCustomer() {
+		return customerService.getAll();
 	}
 	
 }
