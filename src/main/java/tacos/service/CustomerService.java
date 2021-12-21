79934ffb0549d02b89b3ab42017373f2e64ba191
ernest_Z6 <zhang.ernest@outlook.com>
2021-12-21 10:50:45 +0800

    Constructor Injection instead of @Autowired.

--- src/main/java/tacos/service/CustomerService.java
+++ src/main/java/tacos/service/CustomerService.java
@@ -1,16 +1,17 @@
 package tacos.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
 
 import java.util.List;
 import tacos.entity.Customer;
 import tacos.repository.CustomerRepository;
 
 @Service
+@AllArgsConstructor
 public class CustomerService {
 
-	@Autowired
 	CustomerRepository customerRepository;
 	
 	public List<Customer> getAll() {
